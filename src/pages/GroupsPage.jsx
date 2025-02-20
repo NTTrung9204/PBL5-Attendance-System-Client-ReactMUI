f1a52e5cc4169b80d209a07d81b54db8464ee49a
Trung Nguyễn <trung9204@gmail.com>
2025-02-20 13:45:46 +0700

    [Feat]: create section toggle

--- src/pages/GroupsPage.jsx
+++ src/pages/GroupsPage.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import Card from '../components/Card';
 import { Box } from '@mui/material';
+import ToggleSection from '../components/ToggleSection';
 
 function GroupsPage() {
-  const groupTeamName_1 = "Browse through the icons below to find the one you need"
-  const groupTeamName_2 = "The search field supports synonyms—for example, try searching for hamburger"
-  const groupTeamName_3 = "18 matching results"
-  const groupTeamName_4 = "2,100+ ready-to-use React Material Icons from the official website."
+  const groupTeamName_1 = "Browse through the icons below to find the one you need";
+  const groupTeamName_2 = "The search field supports synonyms—for example, try searching for hamburger";
+  const groupTeamName_3 = "18 matching results";
+  const groupTeamName_4 = "2,100+ ready-to-use React Material Icons from the official website.";
+
   return (
-    <Box
-      sx={{display: "flex", flexWrap: "wrap", gap: "20px"}}
-    >
-      <Card groupTeamName={groupTeamName_1}></Card>
-      <Card groupTeamName={groupTeamName_2}></Card>
-      <Card groupTeamName={groupTeamName_3}></Card>
-      <Card groupTeamName={groupTeamName_4}></Card>
+    <Box>
+      <ToggleSection label="Classes">
+        <Card groupTeamName={groupTeamName_1}></Card>
+        <Card groupTeamName={groupTeamName_2}></Card>
+        <Card groupTeamName={groupTeamName_3}></Card>
+        <Card groupTeamName={groupTeamName_4}></Card>
+      </ToggleSection>
+
+      <ToggleSection label="Hidden">
+
+      </ToggleSection>
     </Box>
   );
 }
