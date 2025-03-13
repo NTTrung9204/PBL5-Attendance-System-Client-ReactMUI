a0f99124ced141037f694227ad2ea178aae12ea6
LeTrungPhong <letrungphong22122004@gmail.com>
2025-03-13 13:12:03 +0700

    [Fix]: change row of ui attendance

--- src/pages/AttendancePage.jsx
+++ src/pages/AttendancePage.jsx
@@ -48,74 +48,76 @@ export default function AttendancePage() {
                     <Typography sx={styles.information__item}><ScheduleIcon sx={styles.information__item__icon}/> { AttendanceData.time }</Typography>
                 </Box>
             </Box>
-            <Box sx={styles.statistic}>
-                <Box sx={styles.statistic__item} className="br-8">
-                    <Typography sx={styles.statistic__item__title}>Statistic Summary</Typography>
-                    <Box sx={styles.statistic__item__parameter}>
-                        <Box sx={styles.parameter}>
-                            <Typography sx={styles.parameter__title}>N-Students</Typography>
-                            <Typography sx={styles.parameter__number}>{ ListStudentAttendance.length }</Typography>
+            <Box sx={{ display: "flex", gap: "20px" }}>
+                <Box sx={styles.statistic}>
+                    <Box sx={styles.statistic__item} className="br-8">
+                        <Typography sx={styles.statistic__item__title}>Statistic Summary</Typography>
+                        <Box sx={styles.statistic__item__parameter}>
+                            <Box sx={styles.parameter}>
+                                <Typography sx={styles.parameter__title}>N-Students</Typography>
+                                <Typography sx={styles.parameter__number}>{ ListStudentAttendance.length }</Typography>
+                            </Box>
+                            <Box sx={styles.parameter}>
+                                <Typography sx={styles.parameter__title}>Present</Typography>
+                                <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true).length }</Typography>
+                            </Box>
+                            <Box sx={styles.parameter}>
+                                <Typography sx={styles.parameter__title}>Absent</Typography>
+                                <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == false).length }</Typography>
+                            </Box>
                         </Box>
-                        <Box sx={styles.parameter}>
-                            <Typography sx={styles.parameter__title}>Present</Typography>
-                            <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true).length }</Typography>
-                        </Box>
-                        <Box sx={styles.parameter}>
-                            <Typography sx={styles.parameter__title}>Absent</Typography>
-                            <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == false).length }</Typography>
+                    </Box>
+                    <Box sx={styles.statistic__item} className="br-8">
+                        <Typography sx={styles.statistic__item__title}>Present Summary</Typography>
+                        <Box sx={styles.statistic__item__parameter}>
+                            <Box sx={styles.parameter}>
+                                <Typography sx={styles.parameter__title}>Attendance</Typography>
+                                <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true).length }</Typography>
+                            </Box>
+                            <Box sx={styles.parameter}>
+                                <Typography sx={styles.parameter__title}>Early Clock In</Typography>
+                                <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true && item.time <= AttendanceData.time).length }</Typography>
+                            </Box>
+                            <Box sx={styles.parameter}>
+                                <Typography sx={styles.parameter__title}>Late Clock In</Typography>
+                                <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true && item.time > AttendanceData.time).length }</Typography>
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
-                <Box sx={styles.statistic__item} className="br-8">
-                    <Typography sx={styles.statistic__item__title}>Present Summary</Typography>
-                    <Box sx={styles.statistic__item__parameter}>
-                        <Box sx={styles.parameter}>
-                            <Typography sx={styles.parameter__title}>Attendance</Typography>
-                            <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true).length }</Typography>
-                        </Box>
-                        <Box sx={styles.parameter}>
-                            <Typography sx={styles.parameter__title}>Early Clock In</Typography>
-                            <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true && item.time <= AttendanceData.time).length }</Typography>
-                        </Box>
-                        <Box sx={styles.parameter}>
-                            <Typography sx={styles.parameter__title}>Late Clock In</Typography>
-                            <Typography sx={styles.parameter__number}>{ ListStudentAttendance.filter(item => item.state == true && item.time > AttendanceData.time).length }</Typography>
-                        </Box>
-                    </Box>
-                </Box>
-            </Box>
 
-            <TableContainer sx={styles.table} component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell sx={{ color: "#828282" }}>Student Name</TableCell>
-                        <TableCell sx={{ color: "#828282" }} align="right">State</TableCell>
-                        <TableCell sx={{ color: "#828282" }} align="right">Time</TableCell>
-                        <TableCell sx={{ color: "#828282" }} align="right">Image _ Atd</TableCell>
-                        <TableCell sx={{ color: "#828282" }} align="right">Confirm _ Atd</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {ListStudentAttendance.map((row) => (
-                        <TableRow
-                            // key={}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">
-                                {row.state == true ? <>{row.time > AttendanceData.time && <span style={{ color: "#8470FF" }}>Late{" / "}</span>}<span style={{color: "#00CC33", fontSize: "13px"}}>Attendance</span></> : <Typography sx={{color: "#FA8072", fontSize: "13px"}}>Absent</Typography>}
-                            </TableCell>
-                            <TableCell align="right">{row.time}</TableCell>
-                            <TableCell align="right">{row.state == true && <Button sx={{ fontSize: "12px" }} color="secondary"><FilterIcon/></Button>}</TableCell>
-                            <TableCell align="right">{row.state == false && <Button sx={{ fontSize: "12px" }}>Confirm</Button>}</TableCell>
+                <TableContainer sx={styles.table} component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ color: "#828282" }}>Student Name</TableCell>
+                            <TableCell sx={{ color: "#828282" }} align="right">State</TableCell>
+                            <TableCell sx={{ color: "#828282" }} align="right">Time</TableCell>
+                            <TableCell sx={{ color: "#828282" }} align="right">Image _ Atd</TableCell>
+                            <TableCell sx={{ color: "#828282" }} align="right">Confirm _ Atd</TableCell>
                         </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                        {ListStudentAttendance.map((row) => (
+                            <TableRow
+                                // key={}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">
+                                    {row.state == true ? <>{row.time > AttendanceData.time && <span style={{ color: "#8470FF" }}>Late{" / "}</span>}<span style={{color: "#00CC33", fontSize: "13px"}}>Attendance</span></> : <Typography sx={{color: "#FA8072", fontSize: "13px"}}>Absent</Typography>}
+                                </TableCell>
+                                <TableCell align="right">{row.time}</TableCell>
+                                <TableCell align="right">{row.state == true && <Button sx={{ fontSize: "12px" }} color="secondary"><FilterIcon/></Button>}</TableCell>
+                                <TableCell align="right">{row.state == false && <Button sx={{ fontSize: "12px" }}>Confirm</Button>}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </Box>
     )
 }
@@ -143,9 +145,11 @@ const styles = {
     },
     statistic: {
         display: "flex",
-        gap: "20px"
+        gap: "20px",
+        flexDirection: "column",
     },
     statistic__item: {
+        minWidth: "400px",
         padding: "10px",
         boxShadow: "0px 1px 2px 0px"
     },
@@ -170,10 +174,10 @@ const styles = {
 
     },
     table: {
-        marginTop: "30px",
+        // marginTop: "30px",
         boxShadow: "0px 1px 2px 0px",
         overflow: "auto",
-        maxHeight: "calc(100vh - 400px)",
+        maxHeight: "calc(100vh - 250px)",
         '&::-webkit-scrollbar': {
             width: '10px',
         },
